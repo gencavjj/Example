42779cdd0fb5826ffc10d2c5cc79161292b2aaf4
Dennis Kalaygian <dennis.kalaygian@Trinity.Transport>
2018-07-22 15:01:49 -0400

    Added to gitignore

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public double add(double x, double y) {
+        return x + y;
+    }
+
+    public double subtract(double x, double y) {
+        return x - y;
+    }
+
+    public double multiply(double x, double y) {
+        return x * y;
+    }
+
+    // TODO: Account for y == 0
+    public double divide(double x, double y) {
+        return x / y;
+    }
+
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculator {
+
+    private Calculator calculator;
+
+    @Before
+    public void init() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd_twoPositiveNumbers() {
+        // Given
+        double predictedAnswer = 5.0;
+
+        // When
+        double returnedAnswer = calculator.add(2.0, 3.0);
+
+        // Then
+        Assert.assertEquals("The numbers are not being added correctly",
+                predictedAnswer, returnedAnswer, 0);
+    }
+
+    @Test
+    public void testAdd_twoNegativeNumbers() {
+        // Given
+        double predictedAnswer = -8.0;
+
+        // When
+        double returnedAnswer = calculator.add(-5.0, -3.0);
+
+        // Then
+        Assert.assertEquals("The numbers are not being added correctly",
+                predictedAnswer, returnedAnswer, 0);
+    }
+
+}
